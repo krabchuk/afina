c9e80066424a86edf926d322d6e780b686c63777
krabchuk <krabchuk@gmail.com>
2019-02-26 00:26:13 +0300

    NEW: LRU storage new architecture

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -1,20 +1,82 @@
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 #include "SimpleLRU.h"
 
 namespace Afina {
 namespace Backend {
+
+void SimpleLRU::deque::pop_back() {
+  deque_list *_tail_prev = _tail->_prev;
+  if (_tail_prev) {
+    _tail->_prev->_next.reset();
+  }
+  else {
+    _head.reset();
+  }
+  _tail = _tail_prev;
+}
+
+void SimpleLRU::deque::push_front(const std::string &key) {
+  std::unique_ptr<deque_list> old_head (std::move(_head));
+  _head.reset(new deque_list (key));
+  _head->_next = std::move(old_head);
+  if (_head->_next)
+    _head->_next->_prev = _head.get();
+  if (_tail == nullptr)
+    _tail = _head.get();
+}
+
+void SimpleLRU::deque::erase(const std::string &key) {
+  // We suppose, that this key exists
+  deque_list *curr = _head.get();
+  int flag = 0;
+  while (curr) {
+    if (curr->_key == key) {
+      flag = 1;
+      break;
+    }
+    curr = curr->_next.get();
+  }
+  if (!flag)
+    throw std::invalid_argument("Key doesn't exist " + key);
+
+  if (curr->_next) {
+    curr->_next->_prev = curr->_prev;
+    if (curr->_prev) {
+      curr->_prev->_next = std::move(curr->_next);
+    }
+    else {
+      _head = std::move(curr->_next);
+    }
+  }
+  else {
+    pop_back();
+  }
+}
+
+void SimpleLRU::deque::update(const std::string &key) {
+  if (_head->_key == key)
+    return;
+  erase(key);
+  push_front(key);
+}
 
 bool SimpleLRU::PutItem(const std::string &key, const std::string &value) {
   std::size_t additional_size = key.size() + value.size();
   if (additional_size > _max_size)
     return false;
 
-  while (_actual_size + additional_size > _max_size)
-    Delete(_lru_history.back());
+  while (_actual_size + additional_size > _max_size) {
+    auto &key_for_delete = _lru_history->back();
+    _actual_size = _actual_size - key_for_delete.size() - _lru_storage[key_for_delete].size();
+    _lru_storage.erase(key_for_delete);
+    _lru_history->pop_back();
+  }
 
+  _actual_size += additional_size;
   _lru_storage.insert(std::make_pair(key, value));
-  _lru_history.push_front(std::ref(_lru_storage[key]));
+  _lru_history->push_front(_lru_storage.find(key)->first);
   return true;
 }
 
@@ -34,15 +96,6 @@ bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) {
   return PutItem(key, value);
 }
 
-void SimpleLRU::RemoveKey(const std::string &key) {
-  for (unsigned int i = 0; i < _lru_history.size(); ++i) {
-    if (_lru_history[i].get() == key) {
-      _lru_history.erase(_lru_history.begin() + i);
-      return;
-    }
-  }
-}
-
 // See MapBasedGlobalLockImpl.h
 bool SimpleLRU::Set(const std::string &key, const std::string &value) {
   auto item = _lru_storage.find(key);
@@ -52,13 +105,10 @@ bool SimpleLRU::Set(const std::string &key, const std::string &value) {
   if (_actual_size - item->second.size() + value.size() > _max_size)
     return false;
 
-  RemoveKey(item->first);
-
   _actual_size = _actual_size - item->second.size() + value.size();
   _lru_storage.erase(key);
   _lru_storage.insert(std::make_pair(key, value));
-
-  _lru_history.push_front(std::ref(_lru_storage.find(key)->first));
+  _lru_history->push_front(_lru_storage.find(key)->first);
 }
 
 // See MapBasedGlobalLockImpl.h
@@ -67,10 +117,10 @@ bool SimpleLRU::Delete(const std::string &key) {
   if (item == _lru_storage.end())
     return false;
 
-  RemoveKey(item->first);
-
   _actual_size = _actual_size - item->first.size() - item->second.size();
-  _lru_storage.erase(key);
+  _lru_history->erase(key);
+  _lru_storage.erase(item);
+  return true;
 }
 
 // See MapBasedGlobalLockImpl.h
@@ -79,13 +129,7 @@ bool SimpleLRU::Get(const std::string &key, std::string &value) const {
   if (item == _lru_storage.end())
     return false;
 
-  for (unsigned int i = 0; i < _lru_history.size(); ++i) {
-    if (_lru_history[i].get() == key) {
-      _lru_history.erase(_lru_history.begin() + i);
-      break;
-    }
-  }
-  _lru_history.push_front(item->first);
+  _lru_history->update(item->first);
 
   value.assign(item->second);
   return true;
--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -18,11 +18,13 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {
+      _lru_history.reset(new deque ());
+    }
 
     ~SimpleLRU() {
         _lru_storage.clear();
-        _lru_history.clear();
+        _lru_history.reset();
     }
 
     // Implements Afina::Storage interface
@@ -41,6 +43,38 @@ public:
     bool Get(const std::string &key, std::string &value) const override;
 
 private:
+    struct deque {
+      struct deque_list {
+        deque_list *_prev = nullptr;
+        std::unique_ptr<deque_list> _next;
+
+        const std::string &_key;
+
+        explicit deque_list (const std::string &key) : _key(key) {}
+      };
+
+      ~deque() {
+        if (_head) {
+          while (_tail->_prev) {
+            _tail = _tail->_prev;
+            _tail->_next.reset();
+          }
+          _head.reset();
+        }
+
+      }
+
+      std::unique_ptr<deque_list> _head;
+      deque_list *_tail = nullptr;
+
+      void pop_back();
+      void push_front(const std::string &key);
+      void erase(const std::string &key);
+      const std::string &back() const { return _tail->_key; }
+      const std::string &front() const { return _head->_key; }
+      void update(const std::string &key);
+    };
+
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
     std::size_t _max_size;
@@ -50,14 +84,13 @@ private:
     std::size_t _actual_size = 0;
 
     // Main data storage
-    std::unordered_map<std::string, const std::string> _lru_storage;
+    std::unordered_map<std::string, std::string> _lru_storage;
 
     // Usage history storage.
     // New elements go to head
-    mutable std::deque<std::reference_wrapper<const std::string>> _lru_history;
+    std::unique_ptr<deque> _lru_history;
 
     bool PutItem(const std::string &key, const std::string &value);
-    void RemoveKey(const std::string &key);
 };
 
 } // namespace Backend
