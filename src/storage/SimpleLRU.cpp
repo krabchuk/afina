04406480500a656a2870c21733ada9454daa52b3
Nikita <krabchuk@gmail.com>
2019-03-07 20:19:11 +0300

    FIX: Fix storage tests

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -23,19 +23,27 @@ bool SimpleLRU::PutItem(const std::string &key, const std::string &value) {
     return false;
 
   while (_actual_size + additional_size > _max_size) {
-    _lru_index.erase(_lru_tail->_key);
-    PopBack();
+    Delete(_lru_tail->_key);
   }
 
   _actual_size += additional_size;
 
-  std::unique_ptr<lru_node> old_head (std::move(_lru_head));
-  _lru_head.reset(new lru_node (key, value));
-  _lru_head->_next = std::move(old_head);
-  if (_lru_head->_next)
-    _lru_head->_next->_prev = _lru_head.get();
-  if (_lru_tail == nullptr)
+  if (_lru_head)
+  {
+    std::unique_ptr<lru_node> old_head (std::move(_lru_head));
+    _lru_head.reset(new lru_node (key, value));
+    _lru_head->_next = std::move(old_head);
+    if (_lru_head->_next)
+      _lru_head->_next->_prev = _lru_head.get();
+    if (_lru_tail == nullptr)
+      _lru_tail = _lru_head.get();
+  }
+  else
+  {
+    _lru_head.reset(new lru_node (key, value));
     _lru_tail = _lru_head.get();
+  }
+
 
   _lru_index.insert(std::make_pair(std::ref(_lru_head->_key), std::ref(*_lru_head)));
   return true;
@@ -102,26 +110,25 @@ bool SimpleLRU::Get(const std::string &key, std::string &value) const {
   if (item == _lru_index.end())
     return false;
 
-  auto &curr = item->second.get()._prev ? item->second.get()._prev->_next : _lru_head;
-  value.assign(curr->_value);
+  lru_node &curr = item->second.get();
+  value.assign(curr._value);
 
-  if (curr->_prev) {
-    if (curr->_next) {
-      std::unique_ptr<lru_node> tmp = std::move(curr->_next);
-      tmp->_prev = curr->_prev;
-      curr->_next = std::move(_lru_head);
-      _lru_head = std::move(curr->_prev->_next);
-      curr->_prev->_next = std::move(tmp);
+  if (curr._key != _lru_head->_key) {
+    if (curr._next) {
+      std::unique_ptr<lru_node> tmp = std::move(curr._next);
+      tmp->_prev = curr._prev;
+      curr._next = std::move(_lru_head);
+      _lru_head = std::move(curr._prev->_next);
+      curr._prev->_next = std::move(tmp);
       _lru_head->_next->_prev = _lru_head.get();
-      _lru_head->_prev = nullptr;
     }
     else {
-      _lru_tail = curr->_prev;
-      curr->_next = std::move(_lru_head);
-      curr->_next->_prev = curr.get();
-      _lru_head = std::move(curr->_prev->_next);
-      _lru_head->_prev = nullptr;
+      _lru_tail = curr._prev;
+      curr._next = std::move(_lru_head);
+      _lru_head = std::move(curr._prev->_next);
+      _lru_head->_next->_prev = _lru_head.get();
     }
+    _lru_head->_prev = nullptr;
   }
 
   return true;
